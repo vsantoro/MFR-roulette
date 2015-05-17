d1eb0a9d32b946f0e63680120dfa7ffd33cc4754
JovanMali <jovanmalinovicc@gmail.com>
2015-05-17 20:42:51 +0200

    Implementirao fiziku oko okretanja stola, ostalo je jos da se izgenerise veci broj brzina kojima ce se tocak okretati.

--- src/roulette_server/Game.java
+++ src/roulette_server/Game.java
@@ -87,9 +87,9 @@ public class Game
         croupier.addNewBet(playerId,newBet);
     }
 
-    public synchronized void spinTable()
+    public synchronized void spinTable(double speed)
     {
-        table.spinWheel();
+        table.spinWheel(speed);
     }
 
     public synchronized void updatePlayerMoney(int id, double money) {
