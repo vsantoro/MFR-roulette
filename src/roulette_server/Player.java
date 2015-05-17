bc1f21bce3f7eb9afc386f1afb8ddf46c8f19c01
JovanMali <jovanmalinovicc@gmail.com>
2015-05-17 18:28:09 +0200

    Sinhronizovao updateMoney i slanje poruke o trenutnom stanju.

--- src/roulette_server/Player.java
+++ src/roulette_server/Player.java
@@ -140,7 +140,10 @@ public class Player implements Runnable
         {
             try
             {
-                playerProxy.send(CommunicationCommands.BALANCE +" "+ money);
+                synchronized (this)
+                {
+                     playerProxy.send(CommunicationCommands.BALANCE +" "+ money);
+                }
             }
             catch(IOException e){}
         }
